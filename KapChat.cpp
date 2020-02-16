2e1462091528f1fa27c73d6e6576d93aeb99a73b
Sergey Kozlov <skozlov@nevalink.biz>
2020-02-16 14:58:27 +0300

    Add servo

--- KapChat.cpp
+++ KapChat.cpp
@@ -1,13 +1,19 @@
 #include <Arduino.h>
 #include <WebSocketsClient.h>
 #include <ArduinoJson.h>
+#include <Servo.h>
 
 #include "KapChat.h"
 #include "KapObjects.h"
 #include "KapConfig.h"
 #include "KapNetwork.h"
+#include "KapConfigStruct.h"
 
 KapChat* kapChat;
+
+const int minAngle = 45;
+const int maxAngle = 135;
+const int servoSpeed = 3;
 
 void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
   switch(type) {
@@ -36,6 +42,12 @@ KapChat::KapChat(KapObjects* kapObjects)
 {
   _kapObjects = kapObjects;
   _webSocket = new WebSocketsClient();
+  _servo = new Servo();
+
+  _servo->attach(2);
+  _servo->write(90);
+  delay(1000);
+  _servo->detach();
 
   kapChat = this;
   Serial.println("Chat created");
@@ -56,16 +68,17 @@ void KapChat::connect() {
 }
 
 void KapChat::process(){
+  unsigned long now = millis();
   if (started) {
     _webSocket->loop();
   } else {
-    unsigned long now = millis();
     if (now - _disconnectTime > 5000 || now < _disconnectTime) {
       Serial.println("No websocket connection");
       _disconnectTime = now;
       connect();
     }
   }
+  processServo(now);
 }
 
 void KapChat::processMessage(uint8_t * payload) {
@@ -104,7 +117,6 @@ void KapChat::processMessage(uint8_t * payload) {
       _dep_tp -= 1;
     }
   } else if (type == "watch-ok") {
-    // {"id":1581799638068,"type":"watch-ok","content":{"sessions":[{"department":"cc","count":1},{"department":"tp","count":1}]}}
     JsonArray deps = doc["content"]["sessions"].as<JsonArray>();
     for (int i = 0, l = deps.size(); i < l; i += 1) {
       JsonObject obj = deps[i].as<JsonObject>();
@@ -123,4 +135,110 @@ void KapChat::processMessage(uint8_t * payload) {
   Serial.print("CC: "); Serial.println(_dep_cc);
   Serial.print("TP: "); Serial.println(_dep_tp);
   Serial.print("ANY: "); Serial.println(_dep_any);
+}
+
+void KapChat::processServo(unsigned long now) {
+  if (now - _moveTime < 30 && !(now < _moveTime) ) {
+    return;
+  }
+  _moveTime = now;
+  if (_configParams == NULL) {
+    _configParams = _kapObjects->_config->getConfig();
+  }
+
+  if ( (_dep_cc == 0 || !_configParams->ccWorker)
+    && (_dep_tp == 0 || !_configParams->tpWorker)
+    && _angle < 90  && _direction < 0) {
+    // Влево не надо двигаться больше, меняю направление
+    Serial.println("1 SET DIRECTION TO 1, DEST TO 90");
+    _direction = servoSpeed;
+    _destination = 90;
+  } else if (_dep_any == 0 && _angle > 90 && _direction > 0) {
+    // Вправо не надо двигаться больше, меняю направление
+    _direction = -servoSpeed;
+    _destination = 90;
+    Serial.println("1 SET DIRECTION TO -1, DEST TO 90");
+  } else if (_dep_any == 0
+          && (_dep_cc == 0 || !_configParams->ccWorker)
+          && (_dep_tp == 0 || !_configParams->tpWorker)
+          && (_angle >= 90 && _direction > 0 || _angle <= 90 && _direction < 0)
+          && (_destination == 90)) {
+    _direction = 0;
+    _destination = 90;
+    Serial.println("1 SET DIRECTION TO 0, DEST TO 90");
+    return;
+  }
+
+  // Должен двигаться
+  if (_direction == 0) {
+    // Начало движения
+    // cc и tp влево, any вправо
+    if (_dep_cc > 0 && _configParams->ccWorker
+     || _dep_tp > 0 && _configParams->tpWorker) {
+      _direction = -servoSpeed;
+      _destination = minAngle;
+      Serial.println("SET DIRECTION TO -1, DESTINATION TO 0");
+    } else if (_dep_any > 0) {
+      _direction = servoSpeed;
+      _destination = maxAngle;
+      Serial.println("SET DIRECTION TO 1, DESTINATION TO 180");
+    } else {
+      if (_servo->attached()) {
+        _servo->detach();
+      }
+      return;
+    }
+    Serial.println("ATTACH");
+    _servo->attach(2); // d4
+  }
+
+  Serial.printf("ANGLE: %d, DIRECTION: %d, DESTINATION: %d, cc: %d, tp: %d, any: %d\n", _angle, _direction, _destination, _dep_cc, _dep_tp, _dep_any);
+  if (_angle == _destination // Точное совпадение
+   || _angle < _destination && _direction < 0 // Проскочил точку двигаясь справа
+   || _angle > _destination && _direction > 0 // Проскочил точку двигаясь слева
+    ) {
+    Serial.println("ANGLE EQUAL TO DESTINATION");
+    if (_destination <= minAngle && _direction < 0
+     || _destination >= maxAngle && _direction > 0) {
+      // Если двигались к краям, то движемся к центру
+      _destination = 90;
+      _direction = -_direction;
+      Serial.println("SET DIRECTION TO -DIRECTION, DESTINATION TO 90");
+    } else if (_destination == 90 && _direction > 0) {
+      // Двигался к центру слева
+      if (_dep_any > 0) {
+        _destination = maxAngle;
+        Serial.println("SET DESTINATION TO 180");
+      } else if (_dep_cc > 0 && _configParams->ccWorker
+              || _dep_tp > 0 && _configParams->tpWorker) {
+        _destination = minAngle;
+        _direction = -servoSpeed;
+        Serial.println("SET DIRECTION TO -1, DESTINATION TO 0");
+      } else {
+        _direction = 0;
+        _servo->detach();
+        Serial.println("STOP");
+      }
+    } else if (_destination == 90 && _direction < 0) {
+      // Двигался к центру справа
+      if (_dep_cc > 0 && _configParams->ccWorker
+       || _dep_tp > 0 && _configParams->tpWorker) {
+        _destination = minAngle;
+        Serial.println("SET DESTINATION TO 0");
+      } else if (_dep_any > 0) {
+        _destination = minAngle;
+        _direction = -servoSpeed;
+        Serial.println("SET DIRECTION TO -1, DESTINATION TO 0");
+      } else {
+        _direction = 0;
+        _servo->detach();
+        Serial.println("STOP");
+      }
+    }
+  }
+  if (_direction != 0) {
+    _angle += _direction;
+    Serial.printf("MOVE TO %d\n", _angle);
+    _servo->write(_angle);
+  }
 }
--- KapChat.h
+++ KapChat.h
@@ -1,7 +1,9 @@
 #include <Arduino.h>
 #include <WebSocketsClient.h>
+#include <Servo.h>
 
 #include "KapObjects.h"
+#include "KapConfigStruct.h"
 
 #ifndef KapChat_h
 #define KapChat_h
@@ -15,6 +17,7 @@ class KapChat
     void processMessage(uint8_t * payload);
     KapObjects* _kapObjects;
     WebSocketsClient* _webSocket;
+    Servo * _servo;
     bool connected = false;
   private:
     unsigned long _disconnectTime = 0;
@@ -22,6 +25,15 @@ class KapChat
     int _dep_any = 0;
     int _dep_cc = 0;
     int _dep_tp = 0;
+
+    int _angle = 90;
+    int _direction = 0;
+    int _destination = 90;
+    unsigned long _moveTime = 0;
+    KapConfigParams* _configParams = NULL;
+    
+    
+    void processServo(unsigned long now);
 };
 
 #endif
